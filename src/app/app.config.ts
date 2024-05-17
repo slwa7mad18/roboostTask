546b39273024c267d2b16c38774fa4946b1c74da
salwa <soo181199@gmail.com>
2024-05-17 23:28:37 +0300

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en-US', 'ar-EG'],
+          defaultLang: 'en-US',
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })]
 };
